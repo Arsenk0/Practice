6c19fadb8c92d966f15338503a4a41c038cdbfc2
Arsen Khomiak <arsen.khomiak@gmail.com>
2025-04-10 18:36:54 +0300

    Updated model.py

--- model.py
+++ model.py
@@ -1,19 +1,20 @@
-# model.py
-
 import torch
 import torch.nn as nn
 from torchvision import models
 
+
 class ArtifactDetector(nn.Module):
     def __init__(self):
         super(ArtifactDetector, self).__init__()
-        # Завантажуємо попередньо навчений ResNet18
+        # Завантажуємо ResNet18 без останнього шару
         self.resnet = models.resnet18(pretrained=True)
-        # Замінюємо останній шар для бінарної класифікації
+
+        # Заміняємо останній шар на 1 вихідний нейрон для бінарної класифікації
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 1)
+
+        # Sigmoid для бінарної класифікації
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.resnet(x)
-        x = self.sigmoid(x)
-        return x
+        return self.sigmoid(x)
